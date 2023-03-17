eb4e22134cfeeced91f218571ec1ef8b9a02637f
Absolute-Value <ji.kei1220@gmail.com>
2023-03-18 02:49:19 +0900

    Update

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,15 +1,19 @@
 function showDetails(button) {
-    var popup = document.createElement("div");
-    popup.classList.add("popup");
+    var popup = document.getElementById("popup");
+    popup.innerHTML = "";
+    var popupContent = document.createElement('ul');
+    popupContent.style.backgroundColor = "var(--entry)";
+    popupContent.style.marginTop = "0px";
+    popupContent.style.padding = "3%";
+    popupContent.style.borderRadius = "20px";
 
-    var closeButton = document.createElement("div");
-    closeButton.classList.add("close");
-    closeButton.innerHTML = "x";
-    closeButton.addEventListener("click", closePopup);
+    var skills = document.getElementsByClassName("skill");
+    for (i=0;i<skills.length;i++) {
+        skills[i].style.backgroundColor = "var(--theme)";
+    }
 
-    var popupContent = document.createElement("div");
-    popupContent.classList.add("popup-content");
     spanText = button.querySelector("span").innerHTML;
+    button.style.backgroundColor = "var(--entry)";
     switch (spanText) {
         case 'Python':
             popupContent.innerHTML = '<li>最も使用している言語です．<a href="https://github.com/Absolute-Value/Automate-the-boring-stuff-with-python" class="bachelor" target="_blank"><b>繰り返しの作業の自動化</b></a>や<a href="https://github.com/Absolute-Value/python-GUI"  class="other" target="_blank"><b>GUIソフトの制作</b></a>，<a href="https://github.com/Absolute-Value/Cloth-Anomaly-Detection-pytorch" class=labo target="_blank"><b>Deep Learning</b></a>で使用しています．</li>';
@@ -28,14 +32,16 @@ function showDetails(button) {
             break
         case 'HTML':
         case 'CSS':
-        case 'JavaScript':
             popupContent.innerHTML = '<li>本ページはjekyllを用いて，既存のテーマに頼らずhtmlとCSSから作成しました．</li>';
-            popupContent.innerHTML += '<li>最近では，ダークモードや下にあるGitHubのリポジトリの取得をJavaScriptを用いて作成しました．</li>';
             popupContent.innerHTML += '<li>ページの評判が良く，後輩も使うようになりました！採用実績は<a href="https://ia-gu.github.io/index" class="labo" target="_blank"><b>コチラ</b></li>';
+            break
+        case 'JavaScript':
+            popupContent.innerHTML += '<li>本ページにおけるダークモードの実装や下にあるGitHubのリポジトリの取得をしています．</li>';
             break
         case 'Ubuntu':
             popupContent.innerHTML = '<li>GPUノード用のOSです．無料なのに色々できて便利です．</li>';
             popupContent.innerHTML += '<li>最近は<a href="https://github.com/Absolute-Value/gpu-setup-20.04" class="other" target="_blank"><b>セットアップ</b></a>や修理をしています．</li>';
+            break
         case 'Docker':
             popupContent.innerHTML = '<li>GPUノードの個別の環境構築に使用しています．</li>';
             popupContent.innerHTML += '<li>これまでよくわからず使っていましたが，最近はお友達になりました．リポジトリはセキュリティの都合上，非公開です．</li>';
@@ -44,12 +50,5 @@ function showDetails(button) {
             popupContent.innerHTML = spanText;
     }
 
-    popup.appendChild(closeButton)
     popup.appendChild(popupContent);
-    document.body.appendChild(popup);
-}
-
-function closePopup() {
-    var popup = document.querySelector(".popup");
-    popup.parentNode.removeChild(popup);
 }
